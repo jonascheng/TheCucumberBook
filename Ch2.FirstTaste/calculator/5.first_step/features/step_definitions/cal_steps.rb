04cda6b6eb2f9bb3a94654017078ec86f9840df1
Jonas Cheng <jonas.cheng@gmail.com>
2015-08-23 18:07:19 +0800

    Example and homework for Ch2

---
+++ Ch2.FirstTaste/calculator/5.first_step/features/step_definitions/cal_steps.rb
@@ -0,0 +1,11 @@
+Given /^the input "([^"]*)"$/ do |input|
+  @input = input
+end
+
+When /^the cal is run$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the output should be "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
---
+++ Ch2.FirstTaste/calculator/homework/features/division.feature
@@ -0,0 +1,13 @@
+Feature: Division
+
+  Scenario Outline: Divide two numbers
+    Given the input "<input>"
+    When the cal is run
+    Then the output should be "<output>"
+
+  Examples:
+    | input | output |  
+    | 2/2   | 1      |  
+    | 99/2  | 49     |  
+
+
