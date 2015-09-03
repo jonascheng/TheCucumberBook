839b8a489030afc5c4f9487060d33df5e0961acf
Anderson Chen <AndersonChen@Andersons-MacBook-Air.local>
2015-09-03 22:30:38 +0800

    Fix example calculation logic

--- Ch4.StepDefinitions/06_same_meaning_but_different_wording/features/step_definitions/steps.rb
+++ Ch4.StepDefinitions/06_same_meaning_but_different_wording/features/step_definitions/steps.rb
@@ -1,27 +1,27 @@
 Given /^班級人數(\d+)人$/ do |headcount|
-  @headcount = headcount
+  @headcount = headcount.to_i
 end
 
 When /^扛爺訂(\d+)份總機$/ do |ordered_count|
-  @ordered_count = ordered_count
+  @ordered_count = ordered_count.to_i
 end
 
 Then /^扛爺收到(\d+)個便當$/ do |received_count|
-  received_count == @ordered_count
+  received_count.to_i == @ordered_count
   puts('總機便當Get!')
 end
 
 When /^扛爺訂(\d+)份, C小姐幫扛爺買(\d+)份$/ do |oper_count, c_count|
-	@ordered_count = oper_count + c_count
+	@ordered_count = oper_count.to_i + c_count.to_i
 end
 
 Then /^扛爺收到(\d+)個便當, 揪咪~$/ do |received_count|
-  received_count == @ordered_count
+  received_count.to_i == @ordered_count
   puts('愛心便當+總機便當Get!')
 end
 
 Then /^扛爺收到(\d+)個總機便當$/ do |received_count|
-  received_count == @ordered_count
+  received_count.to_i == @ordered_count
   puts('"領便當的時候到了"')
 end
 
@@ -30,16 +30,16 @@ When /^全員到齊$/ do
 end
 
 Then /^扛爺發(\d+)個總機便當給大家$/ do |dispatch_count|
-  @dispatch_count = dispatch_count
+  @dispatch_count = dispatch_count.to_i
 end
 
 Then /^扛爺自己留(\d+)份$/ do |keep_count|
-  @keep_count = keep_count
+  @keep_count = keep_count.to_i
 end
 
 Given /^扛爺收到(\d+)個總機便當, (\d+)個愛心便當$/ do |oper_count, c_count|
-  @ordered_count = oper_count + c_count
-  @c_count = c_count
+  @ordered_count = oper_count.to_i + c_count.to_i
+  @c_count = c_count.to_i
 end
 
 When /^全部的人都到齊$/ do
@@ -47,6 +47,6 @@ When /^全部的人都到齊$/ do
 end
 
 Then /^扛爺自己留(\d+)份愛心便當$/ do |keep_count|
-  keep_count == @c_count
+  keep_count.to_i == @c_count
 end
 
